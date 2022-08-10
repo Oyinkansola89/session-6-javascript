0290debc2be125e84c87d1729af1c396cd720cbd
Oyinkansola89 <yinxxs82@gmail.com>
2022-08-10 20:02:15 +0100

    learning functions

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
-function sayHey() {
-  console.log("Hello Everyone");
+const firstName = "Oyinkan";
+const surnName = "Olatunji";
+
+function sayHey(person) {
+  return "girl ";
 }
+sayHey(firstName);
+
+function greeting(person) {
+  return "hey! " + "" + person;
+}
+greeting(surnName);
+
+// arrow functions // const conversation = (person) => "Do you like playing " + person;
+//  conversation('oyinkan') //called here
+//its alwyays const no let when using arrow functions
