395b240adb52166ea8b7e8b5e5f7236747450340
Oyinkansola89 <yinxxs82@gmail.com>
2022-08-19 01:20:22 +0100

    functions

--- index.js
+++ index.js
@@ -12,5 +12,7 @@ function greeting(person) {
 greeting(surnName);
 
 // arrow functions // const conversation = (person) => "Do you like playing " + person;
-//  conversation('oyinkan') //called here
-//its alwyays const no let when using arrow functions
+
+//                          conversation('oyinkan')              //called here
+
+//its alwyays const not let when using arrow functions
