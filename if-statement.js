1f320fa0d6a8c2ce7d90410c705adae7443e0c92
Oyinkansola89 <yinxxs82@gmail.com>
2022-08-25 20:33:00 +0100

    changed grades function to an arrow function with conditional expressions

--- if-statement.js
+++ if-statement.js
@@ -1,24 +1,51 @@
-const iAmQueen = true;
+//const iAmQueen = true;
 
-if (iAmQueen) {
-  console.log("Yes, I am a Queen");
-}
+//if (iAmQueen) {
+// console.log("Yes, I am a Queen");
+//}
 
-function grades(marks) {
+//function grades(marks) {
+//if (marks > 90) {
+// console.log("A+");
+// }
+//}
+
+//if (marks > 80 && marks <= 90) {
+// console.log("A");
+//}
+
+//if (marks > 70 && marks <= 80) {
+//  console.log("B");
+//}
+
+//grades(75);
+
+//arrow function version of grades function:
+const grades = (marks) => {
+  // not using paranthesis as there is only one parameter
   if (marks > 90) {
-    console.log("A+");
+    return "A+";
   }
-}
-
-if (marks > 80 && marks <= 90) {
-  console.log("A");
-}
-
-if (marks > 70 && marks <= 80) {
-  console.log("B");
-}
-
-grades(75);
+  if (marks > 80 && marks <= 90) {
+    return "A";
+  }
+  if (marks > 70 && marks <= 80) {
+    return "B";
+  }
+  if (marks > 60 && marks <= 70) {
+    return "C";
+  }
+  if (marks > 50 && marks <= 60) {
+    return "D";
+  }
+  if (marks > 40 && marks <= 50) {
+    return "E";
+  }
+  if (marks > 30 && marks <= 40) {
+    return "F";
+  }
+};
+console.log(grades(49));
 
 // let count = 0;
 //while (count < 10) {
@@ -28,9 +55,9 @@ grades(75);
 //  count = count + 1;
 //}
 
-for (let i = 1; i < 10; i++) {
-  const randomNumber = Math.floor(Math.random() * 100);
-  console.log("The random number is ", randomNumber);
-}
+//for (let i = 1; i < 10; i++) {
+// const randomNumber = Math.floor(Math.random() * 100);
+//console.log("The random number is ", randomNumber);
+//}
 
 //for loop is much more common than while loops because of potential for infinite loop
--- ternary.js
+++ ternary.js
@@ -1,9 +1,8 @@
 function getFee(isMember) {
-    return (isMember ? '£2.00' : '£10.00')
-};
+  return isMember ? "£2.00" : "£10.00";
+}
 
-console.log('Output from tenary operator')
-
+console.log("Output from tenary operator");
 
 console.log(getFee(true));
 
@@ -13,17 +12,17 @@ console.log(getFee(0));
 
 // If/Else example
 
-function getFeeIfElse(isMember) {
-    if (isMember) {
-        return ('£2.00');
-    }
-    else 
-    { 
-        return ('£10.00');
-    }
-} 
+//function getFeeIfElse(isMember) {
+// if (isMember) {
+//return ('£2.00');
+// }
+//  else
+// {
+//return ('£10.00');
+// }
+//}
 
-console.log('Output from if statement')
-console.log(getFeeIfElse(true));
-console.log(getFeeIfElse(false));
-console.log(getFeeIfElse(0));
+//console.log('Output from if statement')
+//console.log(getFeeIfElse(true));
+//console.log(getFeeIfElse(false));
+//console.log(getFeeIfElse(0));
